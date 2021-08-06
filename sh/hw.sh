5b37501ff3470c3fcfb5e7d282cbf627ba44c355
Russell Klein <fuzzyklein2@gmail.com>
2021-08-06 00:55:55 -0500

    Optionally create virtual environment if it doesn't exist.

--- sh/hw.sh
+++ sh/hw.sh
@@ -1,6 +1,25 @@
 #! /usr/bin/env bash
-source ~/.venv-3.9/bin/activate
+
+virtual=0
+venv=/home/fuzzy/.venv-3.9
+
+function venv() {
+  printf "Virtual environment $venv does not exist.\n"
+  printf "Do you want to create it? (y/n): "
+  read answer;
+  case $answer in
+    y)
+      printf "OK, let's do that then.\n"
+      python3.9 -m venv --system-site-packages --symlinks --upgrade-deps $venv
+      virtual=0
+  esac
+}
+
+if [ ! -d ${venv} ]; then
+   virtual=1; venv a
+fi
+[[ $virtual ]] && source ~/.venv-3.9/bin/activate; printf "venv active\n"
 export PYTHONSTARTUP=hw.startup.py
 export startup=PYTHONSTARTUP
 python3 -m hw $@
-deactivate
+[[ $virtual ]] && deactivate
